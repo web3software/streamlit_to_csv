ca4b1b3bad52c0bececd61450be917e1887b15e1
web3software <154231578+web3software@users.noreply.github.com>
2024-01-06 21:11:58 +0500

    1/6/2024

--- streamlit_to_csv.py
+++ streamlit_to_csv.py
@@ -4,9 +4,16 @@ from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 import pandas as pd
 import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.by import By
+
 load_dotenv()
-# access_key = os.getenv('discord_authorization_key')
-access_key = st.secrets['discord_authorization_key']
+access_key = os.getenv('discord_authorization_key')
+# access_key = st.secrets['discord_authorization_key']
 
 def parse_timestamp(timestamp_str):
     try:
@@ -77,54 +84,135 @@ def retrieve_messages_from_channel(channel_id, server_name, channel_name, header
         print(f"Error retrieving messages from channel {channel_id}. Status code: {r_channel.status_code}")
         return messages
 
-
 def download_data():
-    data = {'server': [], 'channel': [], 'author': [], 'original_name': [], 'message': [], 'timestamp': []}
-    group_channels = [
-    ('884204406189490176', ['894619517441957908', '895350107137011723', '1174476193165226004', '955488909436014722', '1168298193646276671']),
-    ('905908516894670928', ['1014574494502891551', '1100410569892307095', '905962797656055919', '1014989330177077370']),
-    ('1131672987502915704', ['1131672988023005206']),
-    ('1189002289885749378', ['1189002290426806325'])
-]
-    headers = {
-    'Authorization': access_key
-    }
-    for group_id, channel_ids in group_channels:
-        server_name = get_server_name(group_id, headers)
+    with st.spinner("Downloading data..."):
+        data = {'server': [], 'channel': [], 'author': [], 'original_name': [], 'message': [], 'timestamp': []}
+        group_channels = [
+        ('884204406189490176', ['894619517441957908', '895350107137011723', '1174476193165226004', '955488909436014722', '1168298193646276671']),
+        ('905908516894670928', ['1014574494502891551', '1100410569892307095', '905962797656055919', '1014989330177077370']),
+        ('1131672987502915704', ['1131672988023005206']),
+        ('1189002289885749378', ['1189002290426806325'])
+    ]
+        headers = {
+        'Authorization': access_key
+        }
+        for group_id, channel_ids in group_channels:
+            server_name = get_server_name(group_id, headers)
 
-        r_group = requests.get(f'https://discord.com/api/v9/guilds/{group_id}/channels', headers=headers)
-        if r_group.status_code == 200:
-            json_data_group = r_group.json()
-            for channel_id in channel_ids:
-                found_channel = next((channel for channel in json_data_group if channel['id'] == channel_id), None)
-                if found_channel:
-                    channel_name = found_channel['name']
-                    
-                    messages = retrieve_messages_from_channel(channel_id, server_name, channel_name, headers)
-                    for message in messages:
-                        content = (message['content'] if message['content'].strip() != "" 
-                        else (message['attachments'][0]['url'] if message['attachments'] else "<Empty Message>"))
-                        data['server'].append(server_name)
-                        data['channel'].append(channel_name)
-                        data['author'].append(message['author']['username'])
-                        data['original_name'].append(message.get('author', {}).get('member', {}).get('nick', message['author']['username']))
-                        data['message'].append(content)
-                        data['timestamp'].append(message['timestamp'])
-                else:
-                    print(f"Channel with ID {channel_id} not found in the group {group_id}. Skipping...")
+            r_group = requests.get(f'https://discord.com/api/v9/guilds/{group_id}/channels', headers=headers)
+            if r_group.status_code == 200:
+                json_data_group = r_group.json()
+                for channel_id in channel_ids:
+                    found_channel = next((channel for channel in json_data_group if channel['id'] == channel_id), None)
+                    if found_channel:
+                        channel_name = found_channel['name']
+                        
+                        messages = retrieve_messages_from_channel(channel_id, server_name, channel_name, headers)
+                        for message in messages:
+                            content = (message['content'] if message['content'].strip() != "" 
+                            else (message['attachments'][0]['url'] if message['attachments'] else "<Empty Message>"))
+                            data['server'].append(server_name)
+                            data['channel'].append(channel_name)
+                            data['author'].append(message['author']['username'])
+                            data['original_name'].append(message.get('author', {}).get('member', {}).get('nick', message['author']['username']))
+                            data['message'].append(content)
+                            data['timestamp'].append(message['timestamp'])
+                    else:
+                        print(f"Channel with ID {channel_id} not found in the group {group_id}. Skipping...")
 
-    df = pd.DataFrame(data)
-    return df
+        df = pd.DataFrame(data)
+        return df
 
-# Streamlit UI
+def scrape_article_info(url, chrome_options):
+    # response = requests.get(url)
+    # article_soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Pass chrome_options to webdriver
+    with st.spinner("Scrapping data..."):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.get(url)  
+        driver.implicitly_wait(5)
+
+        # Extract article name
+        article_name_element = driver.find_elements(By.TAG_NAME, 'h1')
+        article_name = article_name_element[0].text.strip()
+
+        # Extract article date
+        time_element = driver.find_element(By.XPATH, "//time[@datetime]")
+        article_date = time_element.text.strip()
+
+        # Extract image URL
+        div_element_image = driver.find_element(By.XPATH, "//div[contains(@class, 'gg-dark:p-1')]")
+        img_element = div_element_image.find_element(By.TAG_NAME, 'img')
+        img_url = img_element.get_attribute('src')
+
+        # Extract article content
+        div_element = driver.find_element(By.XPATH, "//div[contains(@class, 'grid grid-cols-1 md:grid-cols-8 unreset post-content md:pb-20')]")
+        p_elements = div_element.find_elements(By.XPATH, ".//p[contains(@class, 'font-meta-serif-pro scene:font-noto-sans scene:text-base scene:md:text-lg font-normal text-lg md:text-xl md:leading-9 tracking-px text-body gg-dark:text-neutral-100')]")
+        article_content = '\n'.join([p.text.strip() for p in p_elements])
+
+        driver.quit()
+
+        return article_name, article_date, img_url, article_content
+
+# Function to run code for Tab 1
+def run_tab1():
+    st.subheader("Tab 1: Current Script")
+    if st.button("Download Data"):
+        df = download_data()
+        st.write("Downloaded data:")
+        st.write(df)
+
+        # Save data to Excel
+        excel_filename = "discord_data.xlsx"
+        df.to_excel(excel_filename, index=False)
+        st.success(f"Data saved to {excel_filename}")
+
+# Function to run code for Tab 2
+def run_tab2():
+    st.title("Article Information")
+
+    base_url = 'https://decrypt.co/news'
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_containers = soup.find_all('div', class_='mb-5 pb-5 last-of-type:mb-0')
+
+    # Set up Chrome options for headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Add this line for headless mode
+
+    for container in article_containers:
+        link = container.find('a', class_='linkbox__overlay')
+        if link:
+            href = link.get('href')
+            full_url = f"https://decrypt.co/{href}" 
+
+            article_name, article_date, img_url, article_content = scrape_article_info(full_url, chrome_options)
+
+            # Display article information in a tabular form
+            st.write(f"## {article_name}")
+            st.write(f"**Date:** {article_date}")
+            st.image(img_url, caption="Image", use_column_width=True)
+            st.write("### Article Content:")
+            st.write(article_content)
+
+            st.markdown("---")  # Add a horizontal line between articles
+
+# Function code for the second tab...
+def run_tab3():
+    st.subheader("Tab 3: Other Code")
+    # Function code for the second tab...
+
+# Main Streamlit UI
 st.title("Discord Data Downloader")
 
-if st.button("Download Data"):
-    df = download_data()
-    st.write("Downloaded data:")
-    st.write(df)
-    
-    # Save data to Excel
-    excel_filename = "discord_data.xlsx"
-    df.to_excel(excel_filename, index=False)
-    st.success(f"Data saved to {excel_filename}")
+# Create tabs using st.selectbox
+selected_tab = st.selectbox("Select Tab", ["Discord", "News","YouTube"])
+
+# Display content based on the selected tab
+if selected_tab == "Discord":
+    run_tab1()
+elif selected_tab == "News":
+    run_tab2()
+elif selected_tab == "YouTube":
+    run_tab3()
