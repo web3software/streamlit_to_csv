4e1642f867b5652184aca29b3b1a398efaa7dda7
web3software <154231578+web3software@users.noreply.github.com>
2024-01-09 00:26:16 +0500

    1/9/2024

--- streamlit_to_csv.py
+++ streamlit_to_csv.py
@@ -8,23 +8,24 @@ import shutil
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.by import By
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+import googleapiclient.discovery
 
 load_dotenv()
 access_key = os.getenv('discord_authorization_key')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service_key.json'
 # access_key = st.secrets['discord_authorization_key']
 
 @st.cache_resource(show_spinner=False)
 def get_logpath():
     return os.path.join(os.getcwd(), 'selenium.log')
 
-
 @st.cache_resource(show_spinner=False)
 def get_chromedriver_path():
     return shutil.which('chromedriver')
-
 
 @st.cache_resource(show_spinner=False)
 def get_webdriver_options():
@@ -45,11 +46,9 @@ def get_webdriver_service():
     )
     return service
 
-
 def delete_selenium_log(logpath):
     if os.path.exists(logpath):
         os.remove(logpath)
-
 
 def show_selenium_log(logpath):
     if os.path.exists(logpath):
@@ -196,6 +195,90 @@ def scrape_article_info(url):
 
         return article_name, article_date, img_url, article_content
 
+def get_channel_info(api_key, channel_id):
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id=channel_id
+    )
+
+    try:
+        response = request.execute()
+        return response
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def get_latest_videos(api_key, channel_id, max_results=5):
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+
+    request = youtube.search().list(
+        part="snippet",
+        channelId=channel_id,
+        order="date",
+        type="video",
+        maxResults=max_results
+    )
+
+    try:
+        response = request.execute()
+        return response.get("items", [])
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+def get_english_subtitles(video_id):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+        return transcript
+    except Exception as e:
+        print(f"An error occurred while fetching English subtitles: {e}")
+        return []
+
+def get_youtuber_info(api_key, channel_id):
+    channel_info = get_channel_info(api_key, channel_id)
+    if channel_info:
+        return {
+            'id': channel_info['items'][0]['id'],
+            'title': channel_info['items'][0]['snippet']['title'],
+            'subscriber_count': channel_info['items'][0]['statistics']['subscriberCount']
+        }
+    else:
+        return None
+
+def fetch_video_details(video_url, developer_key, channel_id):
+    try:
+        yt = YouTube(video_url)
+        subtitles = get_subtitles_string(yt.video_id)
+        youtuber_info = get_youtuber_info(developer_key, channel_id)
+        st.subheader("\nVideo Details:")
+        st.write(f"Title: {yt.title}")
+        st.write(f"YouTuber Name: {youtuber_info['title']}")
+        st.write(f"Published At: {yt.publish_date}")
+        st.write(f"Duration: {yt.length} seconds")
+        st.write(f"Views: {yt.views}")
+        st.subheader("Subtitles")
+        st.write(subtitles)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_subtitles_string(video_id):
+    subtitles = get_english_subtitles(video_id)
+    if subtitles:
+        # Concatenate subtitles into a string
+        return '\n'.join(entry['text'] for entry in subtitles)
+    else:
+        return "No English subtitles found."
+
+
 # Function to run code for Tab 1
 def run_tab1():
     st.subheader("Tab 1: Current Script")
@@ -237,18 +320,107 @@ def run_tab2():
 # Function code for the second tab...
 def run_tab3():
     st.subheader("Tab 3: Other Code")
-    # Function code for the second tab...
+    with st.spinner("Scrapping data..."):
+        base_url = 'https://www.coindesk.com/tag/news/'
+        response = requests.get(base_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        h6_tags = soup.find_all('h6', class_="typography__StyledTypography-sc-owin6q-0 diMXjy")
+
+        for h6_tag in h6_tags:
+            link = h6_tag.find('a', class_="card-title")
+
+            if link:
+                href = link.get('href')
+                full_url = f'https://www.coindesk.com/{href}'
+                response = requests.get(full_url)
+                article_soup = BeautifulSoup(response.text, 'html.parser')
+
+                article_name_element = article_soup.find('h1', class_="typography__StyledTypography-sc-owin6q-0 bSOJsQ")
+
+                if article_name_element:
+                    article_name = article_name_element.text.strip()
+                    st.write(f'# Article Name: {article_name}')
+
+                    date_time_div = article_soup.find('div', class_="at-created label-with-icon")
+                    if date_time_div:
+                        date_time_span = date_time_div.find('span', class_="typography__StyledTypography-sc-owin6q-0 hcIsFR")
+                        date_time_text = date_time_span.text.strip()
+                        st.write(f'Date and Time: {date_time_text}')
+                    else:
+                        alt_date_time_div = article_soup.find('div', class_="align-right")
+                        alt_date_time_span = alt_date_time_div.find('span', class_="typography__StyledTypography-sc-owin6q-0 hcIsFR")
+                        alt_date_time_text = alt_date_time_span.text.strip() if alt_date_time_span else 'Date and Time not found'
+                        st.write(f'Date and Time (Alternative): {alt_date_time_text}')
+
+                    main_div = article_soup.find('div', class_='featured-imagestyles__FeaturedImageWrapper-sc-ojmof1-0 jGviVP at-rail-aligner at-rail-aligner-fi')
+
+                    if main_div:
+                        picture_tag = main_div.find('picture', class_='responsive-picturestyles__ResponsivePictureWrapper-sc-1urqrom-0 iLCXlQ')
+
+                        if picture_tag:
+                            img_tag = picture_tag.find('img')
+                            image_url = img_tag['src'] if img_tag else 'Image not found'
+                            st.image(image_url, caption='Article Image', use_column_width=True)
+                        else:
+                            st.write('Image not found within main div.')
+                    else:
+                        main_div2 = article_soup.find('div', class_='featured-imagestyles__FeaturedImageWrapper-sc-ojmof1-0 jGviVP featured-media featured-media-fi')
+                        if main_div2:
+                            picture_tag2 = main_div2.find('picture', class_='responsive-picturestyles__ResponsivePictureWrapper-sc-1urqrom-0 iLCXlQ')
+
+                            if picture_tag2:
+                                img_tag2 = picture_tag2.find('img')
+                                image_url2 = img_tag2['src'] if img_tag2 else 'Image not found'
+                                st.image(image_url2, caption='Article Image', use_column_width=True)
+                            else:
+                                st.write('Image not found within picture tag.')
+                        else:
+                            st.write('Image not in article.')
+
+                    divs = article_soup.find_all('div', class_=["common-textstyles__StyledWrapper-sc-18pd49k-0 eSbCkN"])
+                    st.header('Article Content')
+                    for div in divs:
+                        p_tags = div.find_all('p')
+                        for p_tag in p_tags:
+                            p_text = p_tag.text.strip()
+                            st.write(p_text)
+                else:
+                    st.write('Article Name not found. Moving to the next article.\n')
+
+                # Add a separator between articles
+                st.markdown("---")
+
+# Function code for the second tab...
+def run_tab4():
+    st.subheader("Tab 4: Other Code")
+    with st.spinner("Scrapping data..."):
+        DEVELOPER_KEY = os.getenv('YOUTUBE_API_KEY')
+        CHANNEL_IDS = ['UCfdrZpVbXl_HnmyYYo-N6Ig', 'UCk6jF6z-IZx4H00QTYlHwjw', 'UCMtJYS0PrtiUwlk6zjGDEMA', 'UCKQvGU-qtjEthINeViNbn6A', 'UCqK_GSMbpiV8spgD3ZGloSw', 'UCBCbEDO5tMP6saX9yNU_zYQ','UCN9Nj4tjXbVTLYWN0EKly_Q']
+
+        for channel_id in CHANNEL_IDS:
+            latest_videos = get_latest_videos(DEVELOPER_KEY, channel_id)
+            if latest_videos:
+                print(f"\nLatest Videos for Channel {channel_id}:")
+                for video in latest_videos:
+                    video_id = video['id']['videoId']
+                    video_url = f"https://www.youtube.com/watch?v={video_id}"
+                    fetch_video_details(video_url, DEVELOPER_KEY, channel_id)
+            else:
+                print(f"No videos found for Channel {channel_id}.")
 
 # Main Streamlit UI
-st.title("Discord Data Downloader")
+st.title("DATA SCRAPPER")
 
 # Create tabs using st.selectbox
-selected_tab = st.selectbox("Select Tab", ["Discord", "News","YouTube"])
+selected_tab = st.selectbox("Select Tab", ["Discord", "Decrypt News","Coin Desk News","YouTube"])
 
 # Display content based on the selected tab
 if selected_tab == "Discord":
     run_tab1()
-elif selected_tab == "News":
+elif selected_tab == "Decrypt News":
     run_tab2()
-elif selected_tab == "YouTube":
+elif selected_tab == "Coin Desk News":
     run_tab3()
+elif selected_tab == "YouTube":
+    run_tab4()
