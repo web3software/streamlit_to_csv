be814ec94dc7bebde6fcc212e386e183d3bfca1f
web3software <154231578+web3software@users.noreply.github.com>
2024-01-11 23:59:18 +0500

    1/11/2024 (4)

--- streamlit_to_csv.py
+++ streamlit_to_csv.py
@@ -15,9 +15,22 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import googleapiclient.discovery
 
 load_dotenv()
-access_key = os.getenv('discord_authorization_key')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service_key.json'
-# access_key = st.secrets['discord_authorization_key']
+# access_key = os.getenv('discord_authorization_key')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = {
+  "type": st.secrets['type'],
+  "project_id": st.secrets['project_id'],
+  "private_key_id": st.secrets['private_key_id'],
+  "private_key": st.secrets['private_key'],
+  "client_email": st.secrets['client_email'],
+  "client_id": st.secrets['client_id'],
+  "auth_uri": st.secrets['auth_uri'],
+  "token_uri": st.secrets['token_uri'],
+  "auth_provider_x509_cert_url": st.secrets['auth_provider_x509_cert_url'],
+  "client_x509_cert_url": st.secrets['client_x509_cert_url'],
+  "universe_domain": st.secrets['universe_domain']
+}
+
+access_key = st.secrets['discord_authorization_key']
 
 @st.cache_resource(show_spinner=False)
 def get_logpath():
