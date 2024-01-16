4331ad950158fb214b8149f017b9a311fbbb6013
web3software <154231578+web3software@users.noreply.github.com>
2024-01-16 21:23:31 +0500

    1/16/2024 (2)

--- streamlit_to_csv.py
+++ streamlit_to_csv.py
@@ -1004,6 +1004,7 @@ def run_tab9():
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')   
 
     # Use st.spinner to show a loading spinner while the content is being fetched
     with st.spinner("Fetching content..."):
